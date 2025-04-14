39ac5fd1713a4ba34dd44e5ddbf946044ff53732
Tal Avital <tal.avital04@gmail.com>
2025-04-13 21:51:53 -0400

    add glow

--- Pages/script.js
+++ Pages/script.js
@@ -6,6 +6,8 @@ const footer = document.querySelector(".footer");
 
 const footerButton = document.querySelector(".footer-button");
 
+const cursorGlow = document.getElementById("cursor-glow");
+
 // Declare variables
 var lightMode = localStorage.getItem("lightMode") === "false" ? false : true;
 
@@ -22,6 +24,9 @@ function render()
         title.style.setProperty("background-color", "#CCCCCC");
         body.style.setProperty("background-color", "#FFFFFF");
         footer.style.setProperty("background-color", "#BBBBBB");
+
+        cursorGlow.style.backgroundColor = "#00000088";
+        cursorGlow.style.boxShadow = "0 0 10px #00000088";    
     }
     else
     {
@@ -29,9 +34,18 @@ function render()
         title.style.setProperty("background-color", "#777777");
         body.style.setProperty("background-color", "#AAAAAA");
         footer.style.setProperty("background-color", "#888888");
+
+        cursorGlow.style.backgroundColor = "#00ffff88";
+        cursorGlow.style.boxShadow = "0 0 10px #00ffff88";
+        
     }
 }
 
 footerButton.addEventListener("click", () => {
     window.scrollTo({top: 0});
 })
+
+document.addEventListener("mousemove", (e) => {
+    cursorGlow.style.left = e.clientX + "px";
+    cursorGlow.style.top = e.clientY + "px";
+});
